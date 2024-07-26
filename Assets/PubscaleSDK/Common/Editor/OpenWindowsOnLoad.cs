9bada53510c19ec006193a6529d286f565e5f010
mike <duymaibui2k@gmail.com>
2024-07-26 15:37:29 +0700

    create folder pubscale setting

--- Assets/PubscaleSDK/Common/Editor/OpenWindowsOnLoad.cs
+++ Assets/PubscaleSDK/Common/Editor/OpenWindowsOnLoad.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 namespace PubScale.SdkOne
 {
+    using System.IO;
+
     [InitializeOnLoad]
     public static class OpenWindowsOnLoad
     {
         static OpenWindowsOnLoad()
         {
+            if (!Directory.Exists(PubEditorUX.PackageSettingsPath))
+            {
+                Directory.CreateDirectory(PubEditorUX.PackageSettingsFolderPath);
+            }
             PubScaleSettings settings = AssetDatabase.LoadAssetAtPath<PubScaleSettings>(PubEditorUX.PackageSettingsPath);
 
             if (settings == null)
